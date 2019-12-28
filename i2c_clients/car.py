c009e23848348a84b01d4fdc07fe22cfd1816bdf
Daniel O'Shea <daniel.oshea@gmail.com>
2019-12-28 13:51:15 -0600

    Add cli for i2c client

--- i2c_clients/car.py
+++ i2c_clients/car.py
@@ -1,7 +1,8 @@
 import smbus
 import time
-import curses
 import sys
+
+from getkey import getkey, keys
 
 bus = smbus.SMBus(1)
 
@@ -22,20 +23,20 @@ CMD_STOP_AND_TURN_DEGREES = 15
 
 CMD_DELAY_PER_BYTE = 0.1
 
-#def int_to_bytes(val, num_bytes):
-    # Big-endian
-    #return [(val & (0xff << pos*8)) >> pos*8 for pos in reversed(range(num_bytes))]
-
-    #Little-endian
-    #return [(val & (0xff << pos*8)) >> pos*8 for pos in range(num_bytes)]
-
 def long_to_bytes(val):
     bin_str = "{:032b}".format(val)
     byte_strs = list(map(''.join, zip(*[iter(bin_str)] * 8)))
-    # print(byte_strs)
     byte_list = [int(b, 2) for b in byte_strs]
     byte_list.reverse()
     return byte_list
+
+def int_to_bytes(val):
+    bin_str = "{:016b}".format(val)
+    byte_strs = list(map(''.join, zip(*[iter(bin_str)] * 8)))
+    byte_list = [int(b, 2) for b in byte_strs]
+    byte_list.reverse()
+    return byte_list
+
 
 def drive_forward():
     bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_DRIVE_FORWARD, [])
@@ -47,6 +48,7 @@ def drive_forward_distance(distance):
     bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_DRIVE_FORWARD_DISTANCE, distance_bytes)
     time.sleep(CMD_DELAY_PER_BYTE * 5)
 
+
 def drive_backward():
     bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_DRIVE_BACKWARD, [])
     time.sleep(CMD_DELAY_PER_BYTE)
@@ -57,8 +59,10 @@ def drive_backward_distance(distance):
     bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_DRIVE_BACKWARD_DISTANCE, distance_bytes)
     time.sleep(CMD_DELAY_PER_BYTE * 5)
 
-def turn_degrees(angle):
-    bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_STOP_AND_TURN_DEGREES, [angle])
+
+def turn_degrees(degree):
+    degree_bytes = int_to_bytes(degree)
+    bus.write_i2c_block_data(MOTOR_ADDRESS, CMD_STOP_AND_TURN_DEGREES, [degree_bytes])
     time.sleep(CMD_DELAY_PER_BYTE * 3)
 
 
@@ -77,31 +81,67 @@ def stop():
     time.sleep(CMD_DELAY_PER_BYTE)
 
 
-#drive_forward()
-#time.sleep(2)
-#drive_backward()
-#time.sleep(2)
-#turn_left()
-#time.sleep(5)
-#turn_right()
-#time.sleep(5)
-#stop()
+def interactive_loop():
+    last_key = None
+    while True:
+        key = getkey()
+        if key == keys.UP:
+            drive_forward()
+        elif key == keys.DOWN:
+            drive_backward()
+        elif key == keys.LEFT:
+            turn_left()
+        elif key == keys.RIGHT:
+            turn_right()
+        elif key == keys.SPACE:
+            stop()
+        elif key == 'q':
+            break
+        else:
+            pass
 
-drive_forward_distance(1000);
-#time.sleep(2);
-#drive_forward_distance(1000);
-#time.sleep(2);
-drive_backward_distance(1000);
+        last_key = key
 
-drive_forward();
-drive_backward();
-stop();
-#drive_forward_distance(1000);
-#drive_forward_distance(10000);
-#drive_forward_distance(100000);
+    
+def command_loop():
+    while True:
+        cmd = input("> ")
+        parts = cmd.split(':')
+        
+        cmd_type = parts[0]
+        cmd_type = cmd_type.lower()
+        
+        param1 = None
+        if len(parts) >= 2: param1 = parts[1]
+        
+        if cmd_type == 'i':
+            interactive_loop()
+        elif cmd_type == 'f':
+            if param1:
+                drive_forward_distance(int(param1))
+            else:
+                drive_forward()
+        elif cmd_type == 'b':
+            if param1:
+                drive_backward_distance(int(param1))
+            else:
+                drive_backward()
+        elif cmd_type == 'l':
+            if param1:
+                turn_degrees(-int(param1))
+            else:
+                turn_left()
+        elif cmd_type == 'r':
+            if param1:
+                turn_degrees(int(param1))
+            else:
+                turn_right()
+        elif cmd_type == 's':
+            stop()
 
 
-
+if __name__ == "__main__":
+    command_loop()
 
 
 
